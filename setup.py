18276095b976a2a9b06eb1f94b7ac7cdb17b0bfa
Noremax59 <noremax591@gmail.com>
2021-07-10 13:25:37 -0700

    The first commit to a terrible project

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(name="MyMarketNewsPy",
+    version="0.0.1",
+    author="Normax59",
+    author_email="noremax591@gmail.com",
+    description="MyMarketNews USDA package Python package ",
+    url="https://github.com/noremaxidk/MyMarketNewsUSDA"
+    )
