21130c14221d4dc357a7999bfd8725b13f094c58
Jacob Dallas <j.dallas@xspaceapp.com>
2023-08-21 18:43:03 -0500

    - constants updated to incorporate usda commodity information - API Base updated for creating the payload for the Market class - Market class functionality added, need to test.

--- MyMarketNewsUSDA/ApiBase.py
+++ MyMarketNewsUSDA/ApiBase.py
@@ -1,10 +1,11 @@
+import datetime
 from typing import Any
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 from MyMarketNewsUSDA.ApiKey import ApiKey
-from constants import REPORT_API_BASE_URL
+from constants import REPORT_API_BASE_URL, MARKET_API_BASE_URL
 
 
 class ApiBase(ApiKey):
@@ -15,25 +16,98 @@ class ApiBase(ApiKey):
         super().__init__(api_key)
         self.api_type = api_type
 
-    @staticmethod
-    def create_api_url(slug_id: str, begin_date: str = None, end_date: str = None, key: str = None) -> str:
+    def create_api_url(self, **kwargs) -> str:
         """
         Creates the URL for the API call
 
-        :param slug_id: The ID of the report being fetched
-        :param begin_date: The date of the report being fetched
-        :param end_date: The date of the report being fetched
-        :param key: The key of the report being fetched
+        :param kwargs: The keyword arguments for the Market API call
+            can be any of the following if api_type == "report":
+                - slug_id
+                - begin_date
+                - end_date
+                - key
+
+            can be any of the following if api_type == "market":
+                - commodity
+                - region
+                - class_
+                - organic
+                - begin_date
+                - end_date
+
         :return: The URL for the API call
         """
-        _url = REPORT_API_BASE_URL + slug_id
-        if begin_date is not None:
-            _url += f"?q=report_begin_date={begin_date}"
-        if end_date is not None:
-            _url += f"&report_end_date={end_date}"
-        if key is not None:
-            _url += f"&key={key}"
+        if self.api_type == "report":
+            _url = REPORT_API_BASE_URL + kwargs.get('slug_id', '')
+            if kwargs.get('begin_date') is not None:
+                begin_date = kwargs.get('begin_date')
+                if isinstance(begin_date, datetime.date):
+                    begin_date = begin_date.strftime("%m/%d/%Y")
+                _url += f"?q=report_begin_date={begin_date}"
+            if kwargs.get('end_date') is not None:
+                end_date = kwargs.get('end_date')
+                if isinstance(end_date, (datetime.date, datetime.datetime)):
+                    end_date = end_date.strftime("%m/%d/%Y")
+                _url += f"&report_end_date={end_date}"
+            if kwargs.get('key') is not None:
+                _url += f"&key={kwargs.get('key')}"
+        elif self.api_type == "market":
+            # since this is a post, the variables are passed through the POST payload
+            _url = MARKET_API_BASE_URL
+        else:
+            raise NotImplementedError(f"api_type must be either 'report' or 'market', not {self.api_type}. "
+                                      f"This type is not yet implemented")
         return _url
+
+    def set_api_type(self, api_type: str) -> None:
+        """
+        Sets the api_type of the ApiBase class
+        :param api_type: The api_type to be set
+        :return: None
+        """
+        self.api_type = api_type
+
+    def get_api_type(self) -> str:
+        """
+        Gets the api_type of the ApiBase class
+        :return: The api_type
+        """
+        return self.api_type
+
+    @staticmethod
+    def create_payload(**kwargs) -> dict:
+        """
+        Creates the payload for the Market API call
+        :param kwargs: The keyword arguments for the Market API call
+            can be any of the following:
+                - commodity
+                - region
+                - class_
+                - organic
+                - begin_date
+                - end_date
+        :return: The payload for the Market API call
+        """
+        payload = {}
+        if kwargs.get('commodity') is not None:
+            payload['CLASS'] = kwargs.get('commodity')
+        if kwargs.get('region') is not None:
+            payload['REGN'] = kwargs.get('region')
+        if kwargs.get('class_') is not None:
+            payload['COMD'] = kwargs.get('class_')
+        if kwargs.get('organic') is not None:
+            payload['ORGC'] = kwargs.get('organic')
+        if kwargs.get('begin_date') is not None:
+            begin_date = kwargs.get('begin_date')
+            if isinstance(begin_date, datetime.date):
+                begin_date = begin_date.strftime("%m/%d/%Y")
+            payload['DATE'] = [begin_date]
+        if kwargs.get('end_date') is not None:
+            end_date = kwargs.get('end_date')
+            if isinstance(end_date, (datetime.date, datetime.datetime)):
+                end_date = end_date.strftime("%m/%d/%Y")
+            payload['DATE'].append(end_date)
+        return payload
 
     def get_data(self, _url: str, payload: dict = None) -> Any:
         """
--- MyMarketNewsUSDA/Market.py
+++ MyMarketNewsUSDA/Market.py
@@ -1,21 +1,27 @@
+import datetime
+from typing import Union
+
+import pandas as pd
 import requests
 
-# TODO: create class for Market that inherits from ApiBase
+from MyMarketNewsUSDA.ApiBase import ApiBase
+from constants import *
+
 
 base_url = "https://mymarketnews.ams.usda.gov/get_external_api/result"
 
 payload = {
-  "MT": "/3/",
-  "CLASS": "Fruit",
-  "REGN": "National",
-  "DATE": [
-    "08/01/2023",
-    "08/20/2023"
-  ],
-  "COMD": [
-    "All"
-  ],
-  "ORGC": "No"
+    "MT": "/3/",
+    "CLASS": "Fruit",
+    "REGN": "National",
+    "DATE": [
+        "08/01/2023",
+        "08/20/2023"
+    ],
+    "COMD": [
+        "All"
+    ],
+    "ORGC": "No"
 }
 
 response = requests.post(base_url, json=payload)
@@ -23,3 +29,135 @@ if response.status_code == 200:
     print(response.json())
 else:
     response.raise_for_status()
+
+
+class Market(ApiBase):
+    """
+      This class is used to store the data from a single market and provide methods to manipulate that data
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(api_type="market")
+        self.data = None
+        self.commodity = None
+        self.region = None
+        self.class_ = None
+        self.organic = None
+        self.begin_date = None
+        self.end_date = None
+
+        # set the attributes
+        self.set_commodity(kwargs.get("commodity", None))
+        self.set_region(kwargs.get("region", None))
+        self.set_class(kwargs.get("class_", None))
+        self.set_organic(kwargs.get("organic", None))
+        self.set_begin_date(kwargs.get("begin_date", None))
+        self.set_end_date(kwargs.get("end_date", None))
+
+    def set_commodity(self, commodity: str) -> None:
+        """
+        Sets the commodity of the market, then gathers the data for that market
+        :param commodity: The commodity to be set
+        :return: None
+        """
+        commodity = str(commodity).upper()
+        if commodity in FRUIT_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Fruit"
+        elif commodity in ONIONS_AND_POTATOES_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Onions And Potatoes"
+        elif commodity in VEGETABLES_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Vegetables"
+        elif commodity in HERBS_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Herbs"
+        elif commodity in ORNAMENTALS_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Ornamentals"
+        elif commodity in HEMP_COMMODITIES:
+            self.commodity = commodity.capitalize()
+            self.class_ = "Hemp"
+        else:
+            raise ValueError(f"commodity must be one of the following: {COMPOSITE_COMMODITIES}")
+
+    def set_region(self, region: str) -> None:
+        """
+        Sets the region of the market, then gathers the data for that market
+        :param region: The region to be set
+        :return: None
+        """
+        if region not in COMMODITY_REGIONS:
+            raise ValueError(f"region must be one of the following: {COMMODITY_REGIONS}")
+
+        self.region = region
+
+    def set_class(self, class_: str) -> None:
+        """
+        Sets the class of the market, then gathers the data for that market
+        :param class_: The class to be set
+        :return: None
+        """
+        class_ = str(class_).capitalize()
+        if class_ not in COMMODITY_CLASSES:
+            raise ValueError(f"class_ must be one of the following: {COMMODITY_CLASSES}")
+        self.class_ = class_
+
+    def set_organic(self, organic: Union[str, bool]) -> None:
+        """
+        Sets the organic of the market, then gathers the data for that market
+        :param organic: The organic to be set
+        :return: None
+        """
+        if isinstance(organic, bool):
+            self.organic = "Yes" if organic else "No"
+        elif isinstance(organic, str):
+            organic = organic.capitalize()
+            possible_organic_positives = ["Yes", "True", "Y", "T", "1", "All Organic"]
+            possible_organic_negatives = ["No", "False", "N", "F", "0", "No Organic"]
+            possible_organic_other = ["All"]
+            if organic in possible_organic_positives:
+                self.organic = "Yes"
+            elif organic in possible_organic_negatives:
+                self.organic = "No"
+            elif organic in possible_organic_other:
+                self.organic = "All"
+            else:
+                raise ValueError(
+                    f"organic must be one of the following:"
+                    f" {possible_organic_positives + possible_organic_negatives + possible_organic_other}")
+        else:
+            raise TypeError(f"organic must be of type str or bool, not {type(organic)}")
+
+    def set_begin_date(self, begin_date: Union[str, datetime.date, datetime.datetime]) -> None:
+        """
+        Sets the begin_date of the market, then gathers the data for that market
+        :param begin_date: The begin_date to be set
+        :return: None
+        """
+        if isinstance(begin_date, (datetime.date, datetime.datetime)):
+            begin_date = begin_date.strftime("%m/%d/%Y")
+
+        self.begin_date = begin_date
+
+    def set_end_date(self, end_date: Union[str, datetime.date, datetime.datetime]) -> None:
+        """
+        Sets the end_date of the market, then gathers the data for that market
+        :param end_date: The end_date to be set
+        :return: None
+        """
+        if isinstance(end_date, (datetime.date, datetime.datetime)):
+            end_date = end_date.strftime("%m/%d/%Y")
+
+        self.end_date = end_date
+
+    def refresh_data(self) -> None:
+        """
+        Refreshes the data for the given market and stores it in the data attribute as a pandas dataframe
+        """
+        _url = self.create_api_url()
+        _payload = self.create_payload(commodity=self.commodity, region=self.region, class_=self.class_,
+                                       organic=self.organic, begin_date=self.begin_date, end_date=self.end_date)
+        self.data = self.get_data(_url, _payload)
+        self.data = pd.DataFrame(self.data)
